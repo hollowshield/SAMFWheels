0d9d19d17290e3289a3db1f4d9d8fa4b17a9d51b
Hollow Domain <70131064+hollowshield@users.noreply.github.com>
2024-10-28 10:21:16 -0700

    Add files via upload

--- AutomaticMovement.m
+++ AutomaticMovement.m
@@ -2,31 +2,53 @@ global key
 InitKeyboard();
 hasRight = 0;
 speed = 50;
-time = (speed/50)-.2;
+
 
 
 
 while true
-    pause(.1);
-    distance = brick.UltrasonicDist(2);
-    
-    
-   if (distance > 80 && hasRight==0)
-            brick.MoveMotor('AD', -speed);
 
-            pause(.8);
-       
-            brick.MoveMotor('A', speed);
-            brick.MoveMotor('D', -speed);
-            pause(time);
+   distance = brick.UltrasonicDist(2);
+    
+    
+   if (distance > 60 && hasRight==0)
+
+            pause(.5);
+            brick.MoveMotor('A', speed-10);
+            brick.MoveMotor('D', -speed+10);
+            pause(.35/4);
             hasRight = 1;
+           
             
             % move right 
    end
     
     bumps = brick.TouchPressed(4);
     if (bumps == 0)
-        brick.MoveMotor('AD', speed);
+        pause(1)
+        distance = brick.UltrasonicDist(2);
+        if (distance <  15)
+            brick.MoveMotor('A', speed);
+            brick.MoveMotor('D', speed+2);
+            distance = brick.UltrasonicDist(2);
+            % drift left
+        end
+        if (distance < 30 && distance > 20)
+                brick.MoveMotor('A', speed+7);
+                brick.MoveMotor('D', speed);
+                disp('Right');
+                %drift right
+                
+        else 
+                brick.MoveMotor('A', speed+3);
+                brick.MoveMotor('D', speed);
+                % centerish
+        end
+
+   
+
+
+
         pause(2)
         hasRight = 0;
 
@@ -34,16 +56,14 @@ while true
 
     else
         % move left
-        pause(.1);
-
-        brick.MoveMotor('AD', -speed);
-        pause(1);
-
-  
+        pause(.1)
+        brick.MoveMotor('A', -speed);
+        brick.MoveMotor('D', -speed);
+        pause(.8)
             
         brick.MoveMotor('A', -speed);
         brick.MoveMotor('D', speed);
-        pause(time);
+        pause(.45);
      
 
     end
@@ -53,6 +73,8 @@ while true
     switch key
         case 'q'
             brick.StopAllMotors();
+            distance = brick.UltrasonicDist(2);
+
             break;
         
             
