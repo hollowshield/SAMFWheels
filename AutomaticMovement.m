52b9c16b8f8b3b58531206cde04469f08d5b7d9b
Hollow Domain <70131064+hollowshield@users.noreply.github.com>
2024-10-29 15:25:32 -0700

    Add files via upload

--- AutomaticMovement.m
+++ AutomaticMovement.m
@@ -10,66 +10,35 @@ while true
 
    distance = brick.UltrasonicDist(2);
     
-    
-   if (distance > 60 && hasRight==0)
+   % move right  
+   if (distance >= 50 && hasRight==0)
                 
             
-            brick.MoveMotor('A', speed-10);
-            brick.MoveMotor('D', -speed+10);
-            pause(.7);
-            hasRight = 1;
+        rightTurn(brick,speed,distance, hasRight);
            
             
-            % move right 
+           
    end
    if hasRight == 1
-       brick.MoveMotor('A', speed+3);
-       brick.MoveMotor('D', speed);
+       %Checks for right turn once.
+       forward(distance,brick,speed);
        pause(2);
+
    end
+
     bumps = brick.TouchPressed(4);
     if (bumps == 0)
-        distance = brick.UltrasonicDist(2);
-        if (distance <  15)
-            brick.MoveMotor('A', speed);
-            brick.MoveMotor('D', speed+2);
-            distance = brick.UltrasonicDist(2);
-            disp('Left');
-            % drift left
-        end
-        if (distance < 30 && distance > 20)
-                brick.MoveMotor('A', speed+7);
-                brick.MoveMotor('D', speed);
-                disp('Right');
-                %drift right
-                
-        else 
-                brick.MoveMotor('A', speed+3);
-                brick.MoveMotor('D', speed);
-                disp('Center');
-                % centerish
-        end
-
-   
-
-
-
         
+        distance = brick.UltrasonicDist(2);
+        forward(distance,brick,speed);
         hasRight = 0;
+
 
     
 
     else
-        % move left
-        pause(.1)
-        brick.MoveMotor('A', -speed);
-        brick.MoveMotor('D', -speed);
-        pause(.8)
-            
-        brick.MoveMotor('A', -speed);
-        brick.MoveMotor('D', speed);
-        pause(.7);
-     
+        
+        leftTurn(brick,speed);
 
     end
 
@@ -87,7 +56,50 @@ while true
 end
 
 
+function forward(distance, brick,speed)
+        
+        % drift left
+        if (distance <  15)
+            brick.MoveMotor('A', speed);
+            brick.MoveMotor('D', speed+2);
+            
+            disp('Left');
+           
+        end
+        %drift right
+        if (distance < 30 && distance > 20)
+                brick.MoveMotor('A', speed+7);
+                brick.MoveMotor('D', speed);
+                disp('Right');
+                
+        % centerish       
+        else 
+                brick.MoveMotor('A', speed+3);
+                brick.MoveMotor('D', speed);
+                disp('Center');
+                
+        end
+end
 
+
+function leftTurn(brick,speed)
+    % move left
+        brick.MoveMotor('A', -speed);
+        brick.MoveMotor('D', -speed);
+        pause(.8)
+            
+        brick.MoveMotor('A', -speed);
+        brick.MoveMotor('D', speed);
+        pause(.7);
+end
+function rightTurn(brick,speed,distance, hasRight)
+        brick.MoveMotor('A', speed-10);
+        brick.MoveMotor('D', -speed+10);
+        pause(.8);
+        hasRight = 1;
+
+
+end
 
 CloseKeyboard();
     
