c4349de9d44e28d37f65c4c519d727aa4275ad1a
Souradeep Banerjee <rishibanerjee1101@gmail.com>
2024-11-06 09:28:26 -0800

    Added new implementation of red detection

--- AutomaticMovement.m
+++ AutomaticMovement.m
@@ -10,64 +10,64 @@ minBlue = 100;
 
 while true
 
-   distance = brick.UltrasonicDist(2);
-   color_rgb = brick.ColorRGB(3);
-   colors(color_rgb, brick, minRed, minGreen, minBlue);
+   if(color_rgb(1,1) >= minRed)
+        stopAndThink(1, brick);
+        continue
+   else
+       distance = brick.UltrasonicDist(2);
+       color_rgb = brick.ColorRGB(3);
+   
+       colors(color_rgb, brick, minRed, minGreen, minBlue);
 
-   % move right  
-   if (distance >= 60 && hasRight==0)
+       % move right  
+       if (distance >= 60 && hasRight==0)
                 
             
-        rightTurn(brick,speed);
+               rightTurn(brick,speed);
         
-        forward(brick,speed,speed);
-        pause(2.1);
-        hasRight = 1;
+               forward(brick,speed,speed);
+               pause(2.1);
+               hasRight = 1;
             
            
-   end
+       end
    
 
-    pressed = brick.TouchPressed(1);
+           pressed = brick.TouchPressed(1);
     
     
-    % drift 
-        if (distance >15 && distance < 50)
-            colors(color_rgb, brick, minRed, minGreen, minBlue);
-            adjustRight(speed, 0.2, brick);
-            adjustLeft(speed, .2, brick);
-            
-            disp("right");
-            
-        end
-        if (distance <= 15)
-            colors(color_rgb, brick, minRed, minGreen, minBlue);
-            adjustLeft(speed, .2, brick);
-            adjustRight(speed, 0.2, brick);
-            
-            disp("left");
-        end
-   
+           % drift  
+           if (distance >15 && distance < 50)
+               %forward(brick,speed,speed-3);
+               adjustRight(speed, 0.2, brick);
+               adjustLeft(speed, .2, brick);
+        
+               disp("right");
+        
+           end
+           if (distance <= 15)
+               %forward(brick,speed-1,speed+1);
+               adjustLeft(speed, .2, brick);
+               adjustRight(speed, 0.2, brick);
+        
+               disp("left");
+           end
+           hasRight = 0;
+           % drift end
+           if (pressed == 1)
+               leftTurn(brick,speed);
+               disp(pressed);
+           end
+           if key == 'q'
+                   brick.StopAllMotors();
+                   disp(pressed);
+                   disp(distance);
 
-    hasRight = 0;
-    % drift end
-    if (pressed == 1)
-        leftTurn(brick,speed);
-        disp(pressed);
-    end
-
-    
-
-
-    if key == 'q'
-            brick.StopAllMotors();
-            disp(pressed);
-            disp(distance);
-
-            break;
+                   break;
         
             
-    end
+           end
+       end
 end
 
 
@@ -108,7 +108,7 @@ end
 
 
 function adjustLeft(speed, time, brick)
-    stopAndThink(.5, brick);
+    stopAndThink(1, brick);
 
     forward(brick,-speed+1,-speed-1);
 
@@ -124,7 +124,7 @@ function adjustLeft(speed, time, brick)
 end
 
 function adjustRight(speed, time, brick)
-    stopAndThink(.5, brick);
+    stopAndThink(1, brick);
 
     forward(brick,-speed+1,-speed-1);
 
@@ -141,20 +141,16 @@ end
 function colors(color_rgb, brick, minRed, minGreen, minBlue)
     if(color_rgb(1,1) >= minRed)
         stopAndThink(1, brick);
-        disp("red");
     end
-    if(color_rgb(1,2) >= minGreen && color_rgb(1,3) < minBlue && color_rgb(1,1) < minRed)
+    if(color_rgb(1,2) >= minGreen && color_rgb(1,3) < minBlue)
         brick.StopAllMotors();
         for i=1:3
             brick.beep();
             pause(.2);
         end
         pause(1);
-        disp("green");
-        keyboardMovement;
         
     end
-    
     if(color_rgb(1,3) >= minBlue)
         
         brick.StopAllMotors();
@@ -164,8 +160,6 @@ function colors(color_rgb, brick, minRed, minGreen, minBlue)
             pause(0.2);
         end
         pause(1);
-        disp("blue");
-        keyboardMovement;
     end
     
 end
