9bd8725599b72bcad1aefc6f1b2a0c13cd29f330
Pablo Pelaez Fundora <70131064+hollowshield@users.noreply.github.com>
2024-11-25 09:54:23 -0700

    Add files via upload

--- keyboardMovement.m
+++ keyboardMovement.m
@@ -19,10 +19,10 @@ while true
             brick.MoveMotor('A', -speed);
 
         case 'w'
-            brick.MoveMotorAngleAbs('C', 20, 90, 'Brake'); 
+            brick.MoveMotorAngleAbs('C', 20, 50, 'Brake'); 
 
         case 's'
-            brick.MoveMotorAngleAbs('C', 20, -90, 'Brake'); 
+            brick.MoveMotorAngleAbs('C', 20, -50, 'Brake'); 
 
         case  0
             brick.StopMotor('D');
